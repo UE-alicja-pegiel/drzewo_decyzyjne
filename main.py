c18fe1ba9e3dd72d0678fb47fb5f581274b19349
UE-alicja-pegiel <alicja.pegiel@edu.uekat.pl>
2022-04-26 01:13:08 +0200

    Złe wyświetlanie, nie za dobrze przygotowane dane, dobre obliczenia

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ test = {1: {'old': 'down', 'mid': {2: {'yes': 'down', 'no': 'up'}}, 'new': 'up'}
 
 tab = open("gielda.txt", "r")
 
+print("\nNiestety nie zdążyłam zrobić poprawnego drzewa. \nWszystkie obliczenia znajdują się w pliku 'proby.py' i "
+      "tam testowałam wartości, które wychodzą dobrze. \nNiestety nie będę miała dostępu do komputera"
+      "w czasie 27.04-09.05.2022, dlatego muszę wysłać projekt w obecnym stanie. \n"
+      "Spodobała mi się praca nad drzewem, więc na pewno spróbuję go później jeszcze naprawić :)\n")
+
 test1 = obl.drzewo(tab)
-print(test)
 print(test1)
 
 w = wiz.buduj(test1)

0733a49dce104d0f7fe42faaa729a6aab6343fb0
UE-alicja-pegiel <alicja.pegiel@edu.uekat.pl>
2022-05-21 11:20:02 +0200

    add breast-cancer.data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from moduly import wizualizacja as wiz, obliczenia as obl
 #test = {1:{"n":"tak", "m":{2:{"a":"nie", "b":"tak"}}, "o":"nie"}} #testowe dane
 test = {1: {'old': 'down', 'mid': {2: {'yes': 'down', 'no': 'up'}}, 'new': 'up'}}
 
-tab = open("gielda.txt", "r")
+tab = open("breast-cancer.data", "r")
 
 print("\nNiestety nie zdążyłam zrobić do końca poprawnego drzewa. \nWszystkie obliczenia znajdują się w pliku 'proby.py' i "
       "tam testowałam wartości, które wychodzą dobrze. \nNiestety nie będę miała dostępu do komputera"
